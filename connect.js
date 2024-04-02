227e539bffced1f29af94ba786f099e359f06766
renatinhafront <re.saraiva09@hotmail.com>
2024-04-01 22:39:56 -0300

    configuração para conectar no banco

--- connect.js
+++ connect.js
@@ -1,10 +1,24 @@
+const { MongoClient } = require('mongodb');
 const config = require('./config');
 
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
 
+let db;
 async function connect() {
+  if (db) {
+    return db;
+  }
   // TODO: Database Connection
-}
+  const client = new MongoClient(dbUrl);
 
+  try {
+    await client.connect();
+    db = client.db('bq'); // Reemplaza <NOMBRE_DB> por el nombre del db
+    return db;
+  } catch (e) {
+    console.error(e);
+  }
+}
 module.exports = { connect };
+

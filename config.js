83fa7834c1be280d8c5d9f571b943deee30cfa22
renatinhafront <re.saraiva09@hotmail.com>
2024-04-16 18:34:41 -0300

    ajuste na url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb+srv://resaraiva09:QUpDRFY4lvFYszmz@cluster0.eroqsb1.mongodb.net/burger-queen?retryWrites=true&w=majority&appName=Cluster0';
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://bq:secret@localhost:27017/bq?authSource=admin';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';

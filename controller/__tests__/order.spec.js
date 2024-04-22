14b259448b116520c3194436fd8b203e822cf80f
renatinhafront <re.saraiva09@hotmail.com>
2024-04-22 18:17:11 -0300

    teste finalizado dentro da regra de id sequencial

--- controller/__tests__/order.spec.js
+++ controller/__tests__/order.spec.js
@@ -8,7 +8,12 @@ const {
   removeOrder,
 } = require('../order');
 
-jest.mock('../../repository', () => ({
+jest.mock('../../repository/reposi_product.js', () => ({
+  findAll: jest.fn(),
+  findByID: jest.fn(),
+}));
+
+jest.mock('../../repository/reposi_order.js', () => ({
   findAll: jest.fn(),
   findByID: jest.fn(),
   create: jest.fn(),
@@ -22,18 +27,150 @@ const resp = {
   json: jest.fn(),
 };
 
-describe('getOrders', () => {
-  it('Deve obter uma lista de ordens', async () => {
-    const mockOrders = [
+describe('createOrder', () => {
+  it('deve criar um pedido com sucesso', async () => {
+    const mockCreateOrders = [
       {
-        _id: '6621abcd915edebb7e7216c4',
         userId: 15254,
         client: 'Carol Shaw',
         products: [
           {
             qty: 5,
             product: {
-              id: 1214,
+              name: 'Ham and Cheese Sandwich',
+              price: 1000,
+              image: 'https://github.com/Laboratoria/bootcamp/tree/main/projects/04-burger-queen-api/resources/images/sandwich.jpg',
+              type: 'Breakfast',
+              dateEntry: '2022-03-05T18:14:10.000Z',
+              category: 'Padaria',
+              _id: 10,
+            },
+            _id: 5,
+          },
+        ],
+        status: 'pending',
+        dateEntry: '2022-03-05T18:14:10.000Z',
+        dateProcessed: '2022-03-05T18:14:10.000Z',
+        _id: 7,
+        __v: 0,
+      },
+    ];
+
+    productRepository.findByID.mockResolvedValueOnce({ mockCreateOrders });
+    orderRepository.create.mockResolvedValueOnce(mockCreateOrders);
+
+    req = {
+      body: {
+        userId: 15254,
+        client: 'Carol Shaw',
+        products: [
+          {
+            qty: 5,
+            product: {
+              id: 10,
+            },
+          },
+        ],
+      },
+    };
+
+    await createOrder(req, resp);
+
+    expect(resp.status).toHaveBeenCalledWith(201);
+    expect(resp.json).toHaveBeenCalledWith(mockCreateOrders);
+  });
+
+  it('deve retornar dados de entrada invalídos quando body vazio', async () => {
+    // eslint-disable-next-line no-unused-expressions
+    req = {
+      body: {},
+    };
+    await createOrder(req, resp);
+    expect(resp.status).toHaveBeenCalledWith(400);
+    expect(resp.json).toHaveBeenCalledWith({ error: 'Dados de entrada inválidos.' });
+  });
+  it('deve retornar dados de entrada invalídos quando body.prodcuts vazio', async () => {
+    req = {
+      body: {
+        userId: 15254,
+        client: 'Carol Shaw',
+      },
+    };
+    await createOrder(req, resp);
+    expect(resp.status).toHaveBeenCalledWith(400);
+    expect(resp.json).toHaveBeenCalledWith({ error: 'Dados de entrada inválidos.' });
+  });
+
+  it('deve retornar dados de entrada invalídos quando body.prodcuts vazio', async () => {
+    req = {
+      body: {
+        userId: 15254,
+        client: 'Carol Shaw',
+        products: {},
+      },
+    };
+    await createOrder(req, resp);
+    expect(resp.status).toHaveBeenCalledWith(400);
+    expect(resp.json).toHaveBeenCalledWith({ error: 'Dados de entrada inválidos.' });
+  });
+
+  it('deve retornar um erro 404 se o pedido não for encontrado', async () => {
+    orderRepository.findByID.mockImplementationOnce(() => null);
+
+    req = {
+      body: {
+        userId: 15254,
+        client: 'Carol Shaw',
+        products: [
+          {
+            qty: 5,
+            product: {
+              id: 10,
+            },
+          },
+        ],
+      },
+    };
+    await createOrder(req, resp);
+    expect(resp.status).toHaveBeenCalledWith(404);
+    expect(resp.json).toHaveBeenCalledWith({ error: `Produto ${10} não encontrado.` });
+  });
+
+  it('deve retornar um erro 500 se ocorrer um erro inesperado', async () => {
+    productRepository.findByID.mockRejectedValueOnce(new Error('Ocorreu um erro ao processar a requisição.'));
+    orderRepository.create.mockRejectedValueOnce(new Error('Ocorreu um erro ao processar a requisição.'));
+
+    // eslint-disable-next-line no-unused-expressions
+    req = {
+      body: {
+        userId: 15254,
+        client: 'Carol Shaw',
+        products: [
+          {
+            qty: 5,
+            product: {
+              id: 10,
+            },
+          },
+        ],
+      },
+    },
+
+    await createOrder(req, resp);
+    expect(resp.status).toHaveBeenCalledWith(500);
+    expect(resp.json).toHaveBeenCalledWith({ error: 'Ocorreu um erro ao processar a requisição.' });
+  });
+});
+describe('getOrders', () => {
+  it('Deve obter uma lista de ordens', async () => {
+    const mockOrders = [
+      {
+        userId: 15254,
+        client: 'Carol Shaw',
+        products: [
+          {
+            qty: 5,
+            product: {
               name: 'Ham and Cheese Sandwich',
               price: 1000,
               image: 'https://github.com/Laboratoria/bootcamp/tree/main/projects/04-burger-queen-api/resources/images/sandwich.jpg',
@@ -48,6 +185,7 @@ describe('getOrders', () => {
         status: 'pending',
         dateEntry: '2022-03-05T18:14:10.000Z',
         dateProcessed: '2022-03-05T18:14:10.000Z',
+        _id: 7,
         __v: 0,
       },
 
@@ -64,7 +202,7 @@ describe('getOrders', () => {
   });
 
   it('Retornar erro em caso de erro interno', async () => {
-    orderRepository.findAll.mockResolvedValueOnce(null);
+    orderRepository.findAll.mockRejectedValueOnce(new Error('Ocorreu um erro ao processar a requisição.'));
 
     await getOrders(req, resp);
 
@@ -75,26 +213,42 @@ describe('getOrders', () => {
 
 describe('getOrderById', () => {
   it('Deve obter o usuário pelo ID', async () => {
-    // eslint-disable-next-line operator-linebreak
-    const mockUserId = {
-      _id: '6619acbc13832c1f5a8f26eb',
-      email: 'admin@localhost.com',
-      role: 'admin',
+    const mockOrderById = {
+      userId: 15254,
+      client: 'Carol Shaw',
+      products: [
+        {
+          qty: 5,
+          product: {
+            name: 'Ham and Cheese Sandwich',
+            price: 1000,
+            image: 'https://github.com/Laboratoria/bootcamp/tree/main/projects/04-burger-queen-api/resources/images/sandwich.jpg',
+            type: 'Breakfast',
+            dateEntry: '2022-03-05T18:14:10.000Z',
+            category: 'Padaria',
+            _id: 6,
+          },
+          _id: 6,
+        },
+      ],
+      status: 'pending',
+      dateEntry: '2022-03-05T18:14:10.000Z',
+      dateProcessed: '2022-03-05T18:14:10.000Z',
+      _id: 7,
       __v: 0,
     };
 
-    orderRepository.findByID.mockResolvedValueOnce(mockUserId);
+    orderRepository.findByID.mockResolvedValueOnce(mockOrderById);
 
     const req = {
-      params: { uid: '6619acbc13832c1f5a8f26eb' },
+      params: { orderId: 7 },
     };
 
     await getOrderById(req, resp);
 
     // Verificar se a resposta tem status 200
     expect(resp.status).toHaveBeenCalledWith(200);
-    // Verificar se a resposta contém a lista de usuário
-    expect(resp.json).toHaveBeenCalledWith(mockUserId);
+    expect(resp.json).toHaveBeenCalledWith(mockOrderById);
   });
 
   it('Deve retornar um erro 404 quando o ID for inválido', async () => {
@@ -106,67 +260,37 @@ describe('getOrderById', () => {
 
     // Verificar se a resposta tem status 404
     expect(resp.status).toHaveBeenCalledWith(404);
-    // Verificar se a resposta contém a mensagem de erro
-    expect(resp.json).toHaveBeenCalledWith({ error: 'ID de usuário inválido' });
+    expect(resp.json).toHaveBeenCalledWith({ error: 'ID inválido' });
   });
 
-  it('Deve retornar um erro 404 quando o usuário não for encontrado', async () => {
+  it('Deve retornar um erro 404 quando o pedido não for encontrado', async () => {
     orderRepository.findByID.mockResolvedValueOnce(null);
 
     req = {
-      params: { uid: 'banana' },
+      params: { orderId: 7 },
     };
 
     await getOrderById(req, resp);
 
     expect(resp.status).toHaveBeenCalledWith(404);
-    expect(resp.json).toHaveBeenCalledWith({ error: 'Usuário não encontrado.' });
+    expect(resp.json).toHaveBeenCalledWith({ error: 'Pedido não encontrado.' });
   });
 
-  it('Retornar erro em caso de erro interno', async () => {
-    orderRepository.findByEmail.mockResolvedValueOnce(null);
-    orderRepository.create.mockRejectedValueOnce(new Error('Erro interno no servidor'));
+  it('Retornar erro 500 em caso de erro interno', async () => {
+    const mockError = new Error('Error');
+    orderRepository.findByID.mockRejectedValueOnce(mockError);
 
-    req = {
-      body: { email: 'admin@localhost.com', role: 'admin', password: 'xxxx' },
-    };
-
-    await createOrder(req, resp);
+    await getOrderById(req, resp);
 
     expect(resp.status).toHaveBeenCalledWith(500);
     expect(resp.json).toHaveBeenCalledWith({ error: 'Ocorreu um erro ao processar a requisição.' });
   });
 });
 
-describe('createOrder', () => {
-  it('deve criar um pedido com sucesso', async () => {
-    await createOrder(req, resp);
-    expect(orderRepository.create).toHaveBeenCalledWith(req.body);
-    expect(productRepository.findByID).toHaveBeenCalledTimes(2);
-    expect(productRepository.updateStock).toHaveBeenCalledTimes(2);
-    expect(resp.status).toHaveBeenCalledWith(201);
-    expect(resp.json).toHaveBeenCalledWith({ message: 'Pedido criado com sucesso.', order: { id: 'pedido123', ...req.body } });
-  });
-
-  it('deve retornar um erro 404 se algum produto não for encontrado', async () => {
-    productRepository.findByID.mockImplementationOnce(() => null);
-    await createOrder(req, resp);
-    expect(resp.status).toHaveBeenCalledWith(404);
-    expect(resp.json).toHaveBeenCalledWith({ error: `Produto ${orderRepository.product.id} não encontrado.` });
-  });
-
-  it('deve retornar um erro 500 se ocorrer um erro inesperado', async () => {
-    orderRepository.create.mockRejectedValueOnce(new Error('Erro ao criar pedido'));
-    await createOrder(req, resp);
-    expect(resp.status).toHaveBeenCalledWith(500);
-    expect(resp.json).toHaveBeenCalledWith({ error: 'Falha ao criar pedido.' });
-  });
-});
-
 describe('updateOrder', () => {
   it('Deve retornar sucesso quando for alterado o user', async () => {
     const mockOrderUpdated = {
-      _id: '6619acbc13832c1f5a8f26eb',
+      _id: 6,
       email: 'admin2@localhost.com',
       role: 'admin',
       __v: 0,
@@ -175,8 +299,7 @@ describe('updateOrder', () => {
     orderRepository.update.mockResolvedValueOnce(mockOrderUpdated);
 
     req = {
-      params: { uid: '6619acbc13832c1f5a8f26eb' },
-      body: { email: 'admin2@localhost.com', role: 'admin', password: 'xxxx' },
+      params: { orderId: 6 },
     };
 
     await updateOrder(req, resp);
@@ -193,14 +316,14 @@ describe('updateOrder', () => {
     await updateOrder(req, resp);
 
     expect(resp.status).toHaveBeenCalledWith(400);
-    expect(resp.json).toHaveBeenCalledWith({ error: 'ID de usuário inválido' });
+    expect(resp.json).toHaveBeenCalledWith({ error: 'ID inválido' });
   });
 
   it('Deve retornar erro 404 quando o usuário não existe', async () => {
     orderRepository.update.mockResolvedValueOnce(null);
 
     req = {
-      params: { uid: '6619acbc13832c1f5a8f26eb' },
+      params: { orderId: 6 },
       body: { email: 'admin2@localhost.com', role: 'admin', password: 'xxxx' },
     };
 
@@ -210,15 +333,15 @@ describe('updateOrder', () => {
     expect(resp.json).toHaveBeenCalledWith({ error: 'Pedido não encontrado.' });
   });
 
-  it('Retornar erro em caso de erro interno', async () => {
-    orderRepository.findByEmail.mockResolvedValueOnce(null);
-    orderRepository.create.mockRejectedValueOnce(new Error('Erro interno no servidor'));
+  it('Retornar erro 500 em caso de erro interno', async () => {
+    const mockError = new Error('Error');
+    orderRepository.update.mockRejectedValueOnce(mockError);
 
     req = {
       body: { email: 'admin@localhost.com', role: 'admin', password: 'xxxx' },
     };
 
-    await createOrder(req, resp);
+    await updateOrder(req, resp);
 
     expect(resp.status).toHaveBeenCalledWith(500);
     expect(resp.json).toHaveBeenCalledWith({ error: 'Ocorreu um erro ao processar a requisição.' });
@@ -227,23 +350,19 @@ describe('updateOrder', () => {
 
 describe('removeOrder', () => {
   it('Deve retornar 200 ao deletar o order', async () => {
-    const mockUserDelete = {
-      _id: '6619acbc13832c1f5a8f26eb',
-    };
-
-    orderRepository.removeOrder.mockResolvedValueOnce(mockUserDelete);
+    orderRepository.remove.mockResolvedValueOnce({ message: 'Pedido removido com sucesso.' });
 
     req = {
-      params: { orderId: '619acbc13832c1f5a8f26eb' },
+      params: { orderId: 6 },
     };
 
     await removeOrder(req, resp);
 
     expect(resp.status).toHaveBeenCalledWith(200);
-    expect(resp.json).toHaveBeenCalledWith(mockUserDelete);
+    expect(resp.json).toHaveBeenCalledWith({ message: 'Pedido removido com sucesso.' });
   });
 
-  it('Deve retornar um erro 400 quando o pedido o ID for inválido', async () => {
+  it('Deve retornar um erro 400 quando o ID for inválido', async () => {
     req = {
       params: { },
     };
@@ -251,14 +370,14 @@ describe('removeOrder', () => {
     await removeOrder(req, resp);
 
     expect(resp.status).toHaveBeenCalledWith(400);
-    expect(resp.json).toHaveBeenCalledWith({ error: 'ID de pedido inválido' });
+    expect(resp.json).toHaveBeenCalledWith({ error: 'ID inválido' });
   });
 
   it('Deve retornar um erro 404 quando o pedido não for encontrado', async () => {
     orderRepository.remove.mockResolvedValueOnce(null);
 
     req = {
-      params: { orderId: '619acbc13832c1f5a8f26eb' },
+      params: { orderId: 6 },
     };
 
     await removeOrder(req, resp);
@@ -267,10 +386,11 @@ describe('removeOrder', () => {
     expect(resp.json).toHaveBeenCalledWith({ error: 'Pedido não encontrado.' });
   });
 
-  it('Retornar erro em caso de erro interno', async () => {
-    orderRepository.findByID.mockResolvedValueOnce(null);
+  it('Retornar erro 500 em caso de erro interno', async () => {
+    orderRepository.remove.mockRejectedValueOnce(new Error('Ocorreu um erro ao processar a requisição.'));
+
     req = {
-      params: { orderId: '619acbc13832c1f5a8f26eb' },
+      params: { orderId: 6 },
     };
 
     await removeOrder(req, resp);
